908e43c8454f1c6bfd64eb1c4a8bb8f9383af058
rofiadhimPS <muhammadrofiadhim@gmail.com>
2023-08-24 22:21:20 +0700

    Commit Pertama

---
+++ app/src/main/java/expertcoding/belajarandroiddasar/MainActivity.kt
@@ -0,0 +1,11 @@
+package expertcoding.belajarandroiddasar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tugas_layout)
+    }
+}
